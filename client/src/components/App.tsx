206bbce5f1d2b44a5541c57182ba0d77f4eeb178
Ben Ashkenazi <benoson33@gmail.com>
2021-01-26 01:52:21 +0200

    mid compose email page

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,12 +1,27 @@
-import './App.css';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import LeftSidebar from './sidebar/LeftSidebar';
 import Navbar from './navbar/Navbar';
+import ComposeEmailPage from './compose-email-page/ComposeEmailPage';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <LeftSidebar />
+
+      <div className="dividedSection">
+        <LeftSidebar />
+
+        <BrowserRouter>
+          <Switch>
+            <Route path="/myEmails" component={ComposeEmailPage} exact />
+            <Route path="/compose" component={ComposeEmailPage} exact />
+            
+            <Redirect from="/" to="/welcome" exact />
+          </Switch>
+
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
--- client/src/components/compose-email-page/ComposeEmailPage.tsx
+++ client/src/components/compose-email-page/ComposeEmailPage.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import TextField from '@material-ui/core/TextField';
+import './ComposeEmailPage.css';
 
 export default function ComposeEmailPage() {
     return (
-        <div>
-            
+        <div className="composeEmailPage">
+            <h1 className="composeEmailHeader">Compose Your Email</h1>
+            <form className="composeEmailForm">
+                <TextField className="inputField" margin="normal" label="Subject" variant="outlined" />
+
+                <TextField
+                    id="messageInput"
+                    margin="normal"
+                    className="inputField"
+                    label="Compose your message"
+                    multiline
+                    rows={12}
+                    variant="outlined"
+                />
+            </form>
         </div>
     )
 }
