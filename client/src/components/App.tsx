28758b2ca7abfabe80fb061c0df408c07a067bb0
Ben Ashkenazi <benoson33@gmail.com>
2021-01-25 19:16:27 +0200

    t

---
+++ client/src/components/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import LeftSidebar from './sidebar/LeftSidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <LeftSidebar />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/sidebar/LeftSidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LeftSidebar() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
